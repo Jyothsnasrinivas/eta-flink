30c15384bf6aabc608b0e73ac5fcd227ffc3e3e6
Jyothsna Srinivas <jyothsnasrinivas17@gmail.com>
2018-08-28 13:54:21 +0530

    Updated Flink example

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Java
 import Types
+import Prelude hiding (sum)
+import Control.Monad (forM_, when)
+-- import Interop.Java.StringUtils
+import qualified Interop.Java.StringUtils as JS
 
 main :: IO ()
 main = java $ do
-    env <- ExecutionEnvironment <.> getExecutionEnvironment
-    sampleData <- env <.> readTextFile "TODO"
-    counts <- sampleData <.> flatMap (line LineSplitter)
-                <.> groupBy [0]
-                <.> aggregate Aggregations.sum 1
+    env <- getExecutionEnvironment
+    sampleData <- env <.> readTextFile "input.txt"
+    counts <- sampleData <.> flatMap (mkFlatMap lineSplitter)
+                >- returns "Tuple2<String,Integer>"
+                >- groupBy (toJava [0 :: Int])
+                >- returns "int.."
+                >- (aggregate sum 1)
     counts <.> printFlink
-    counts <.> writeAsText "TODO" <.> setParallelism 1
+    counts <.> writeAsText "output.txt" >- setParallelism 1
     env <.> executeFlink "WordCount Example"
+    return ()
 
-    where line = do
-              flatMap value out
-              tokens <- value ---TODO
+    where  lineSplitter :: JString -> Collector (Tuple2 JString JInteger)
+                       -> Java (FlatMapFunction JString (Tuple2 JString JInteger)) ()
+           lineSplitter value out = do
+              let tokens = flip JS.split "\\W+" $ toLowerCase value
+                  jstrings = fromJava tokens :: [JString]
+              forM_ jstrings $ \token -> do
+                when (JS.length token > 0) $ do
+                  tupleFlink <- (newTuple2 token (toJava (1 :: Int) :: JInteger))
+                  out <.> collect tupleFlink
--- src/Types.hs
+++ src/Types.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Types where
 
@@ -13,49 +14,73 @@ data ExecutionEnvironment = ExecutionEnvironment @org.apache.flink.api.java.Exec
     deriving Class
 
 foreign import java unsafe "@static org.apache.flink.api.java.ExecutionEnvironment.getExecutionEnvironment"
-      getExecutionEnvironment :: IO ExecutionEnvironment
+      getExecutionEnvironment :: Java a ExecutionEnvironment
 
 foreign import java unsafe readTextFile
     :: JString -> Java ExecutionEnvironment (DataSource JString)
 
-foreign import java unsafe "execute" executeFlink :: Java ExecutionEnvironment JobExecutionResult
+foreign import java unsafe "execute" executeFlink :: JString -> Java ExecutionEnvironment JobExecutionResult
 ---
 
 data DataSet t = DataSet (@org.apache.flink.api.java.DataSet t)
     deriving Class
 
-foreign import java unsafe flatMap :: (t <: Object, r <: Object)
-      => FlatMapFunction t r -> Java DataSet (FlatMapOperator t r)
+foreign import java unsafe flatMap :: (t <: Object, r <: Object, a <: DataSet t) =>
+    FlatMapFunction t r -> Java a (FlatMapOperator t r)
 
 foreign import java unsafe groupBy
-    :: (t <: Object) => JIntArray -> Java DataSet (UnsortedGrouping t)
+    :: (t <: Object, a <: DataSet t) => JIntArray -> Java a (UnsortedGrouping t)
 
-foreign import java unsafe aggregate
-    :: (t <: Object) => Aggregations -> Int -> Java DataSet (Aggregateoperator t)
+-- foreign import java unsafe aggregate
+--     :: (a <: DataSet t) => Aggregations -> Int -> Java a (AggregateOperator t)
 
-foreign import java unsafe "count" countFlink :: Java DataSet ()
+foreign import java unsafe "count" countFlink :: (a <: DataSet t) => Java a ()
 
-foreign import java unsafe writeAsText :: (t <: Object) => JString -> Java DataSet (DataSink t)
+foreign import java unsafe "print" printFlink :: (a <: DataSet t) => Java a ()
+
+foreign import java unsafe writeAsText :: (t <: Object, a <: DataSet t)
+      => JString -> Java a (DataSink t)
 ---
-data Tuple2 t0 t1 = Tuple2 (org.apache.flink.api.java.tuple.Tuple2 t0 t1)
+data Tuple2 t0 t1 = Tuple2 (@org.apache.flink.api.java.tuple.Tuple2 t0 t1)
     deriving Class
+
+foreign import java unsafe "@new" newTuple2 :: (t0 <: Object, t1 <: Object)
+  => t0 -> t1 -> Java a (Tuple2 t0 t1)
 ---
 data FlatMapFunction t o = FlatMapFunction (@org.apache.flink.api.common.functions.FlatMapFunction t o)
     deriving Class
+
+foreign import java unsafe "@wrapper flatMap" mkFlatMap :: (t <: Object, o <: Object)
+      => (t -> Collector o -> Java (FlatMapFunction t o) ()) -> FlatMapFunction t o
 ---
-data FlatMapOperator in out = FlatMapOperator (@org.apache.flink.api.java.operators.FlatMapOperator in out)
+data FlatMapOperator t o = FlatMapOperator (@org.apache.flink.api.java.operators.FlatMapOperator t o)
     deriving Class
+
+type instance Inherits (FlatMapOperator t o) = '[SingleInputUdfOperator t o (FlatMapOperator t o)]
+
 ---
-data DataSource out = DataSource (@org.apache.flink.api.java.operators.DataSource out)
+data DataSource o = DataSource (@org.apache.flink.api.java.operators.DataSource o)
     deriving Class
+
+type instance Inherits (DataSource o) = '[Operator o (DataSource o)]
 ---
 data UnsortedGrouping t = UnsortedGrouping (@org.apache.flink.api.java.operators.UnsortedGrouping t)
     deriving Class
+
+type instance Inherits (UnsortedGrouping t) = '[Grouping t]
+
+foreign import java unsafe aggregate :: (t <: Object)
+    => Aggregations -> Int -> Java (UnsortedGrouping t) (AggregateOperator t)
+---
+
+data Grouping t = Grouping (@org.apache.flink.api.java.operators.Grouping t)
+    deriving Class
+
 ---
 data Aggregations = Aggregations @org.apache.flink.api.java.aggregation.Aggregations
     deriving Class
 
-type instance Inherits Aggregations = '[Enum Aggregations]
+-- type instance Inherits Aggregations = '[Enum Aggregations]
 
 foreign import java unsafe
   "@static @field org.apache.flink.api.java.aggregation.Aggregations.SUM"
@@ -70,8 +95,31 @@ data Operator out o = Operator (@org.apache.flink.api.java.operators.Operator ou
     deriving Class
 
 type instance Inherits (Operator out o) = '[DataSet out]
-foreign import java unsafe setParallelism :: (o <: Operator out o) -> Int -> Java Operator o
+
+foreign import java unsafe setParallelism :: Int -> Java (DataSink t) (DataSink t)
 
 ---
 data JobExecutionResult = JobExecutionResult @org.apache.flink.api.common.JobExecutionResult
     deriving Class
+----
+data Collector t = Collector (@org.apache.flink.util.Collector t)
+    deriving Class
+
+foreign import java unsafe "@interface" collect :: (t <: Object)
+    => t -> Java (Collector t) ()
+---
+data AggregateOperator t = AggregateOperator (@org.apache.flink.api.java.operators.AggregateOperator t)
+    deriving Class
+
+type instance Inherits (AggregateOperator t) = '[Operator t (AggregateOperator t)]
+
+foreign import java unsafe toLowerCase :: JString -> JString
+
+---
+data SingleInputUdfOperator t out o =  SingleInputUdfOperator (@org.apache.flink.api.java.operators.SingleInputUdfOperator t out o)
+    deriving Class
+
+type instance Inherits (SingleInputUdfOperator t out o) = '[Operator out o]
+
+foreign import java unsafe returns :: (a <: SingleInputUdfOperator t out o, o <: SingleInputUdfOperator t out o)
+  => JString -> Java a o
